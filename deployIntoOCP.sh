fb90a49cc32604012b8ec29966dfd3017919e3f9
user1 <lilim@redhat.com>
2021-01-11 02:43:09 +0000

    updates

--- deployIntoOCP.sh
+++ deployIntoOCP.sh
@@ -1,7 +1,7 @@
-oc new-build fuse7-java-openshift:1.7~https://github.com/likhia/Camel7AmqStreamService.git --name=camel7amqstreamservice
+oc new-build fuse7-java-openshift:1.7~https://github.com/likhia/Camel7AmqStreamService.git --name=amqfusedemo
 
 sleep 60s
 
-oc new-app camel7amqstreamservice:latest
+oc new-app amqfusedemo:latest
 
 oc expose service/camel7amqstreamservice
